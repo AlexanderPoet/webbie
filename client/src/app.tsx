c2fd62eedc0f8df326adbf510f1bda4889d4ce9d
Alexander Poet <alex.a.poet@gmail.com>
2021-12-16 21:41:57 -0500

    init

---
+++ client/src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const App = (): JSX.Element => (
+  <div>Hello olleH A Change</div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: './client/src/app.tsx',
+  output: {
+    path: __dirname + '/public',
+    filename: 'build/app.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  },
+  devtool: 'eval-source-map',
+  mode: 'development'
+}
